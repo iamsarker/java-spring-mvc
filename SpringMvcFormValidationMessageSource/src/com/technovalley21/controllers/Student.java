045551992c70adc813b5f7c3c426817a88174ac5
Md. Shahadat Sarker <me.sarker@gmail.com>
2017-02-14 11:58:57 +0600

    Spring MVC form validation with message source & more annotation
    Spring MVC form validation with message source & more annotation

---
+++ SpringMvcFormValidationMessageSource/src/com/technovalley21/controllers/Student.java
@@ -0,0 +1,76 @@
+package com.technovalley21.controllers;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+
+public class Student {
+	
+	@Pattern(regexp="[^0-9]*")
+	private String name;
+	
+	@Max(26)
+	@Min(15)
+	private int age;
+		
+	/*
+	 * {0} == object property/field name
+	 * {1}, {2}, {3} .... == annotation arguments with alphabetic order. i.e max is upon min.
+	 * 
+	 * */
+	
+	@Size(min=10, max=16)
+	private String nid;
+	
+	@Past // @Future
+	private Date studentDoB;
+	private ArrayList<String> skillList;
+	private Address studentAddress;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getNid() {
+		return nid;
+	}
+	public void setNid(String nid) {
+		this.nid = nid;
+	}
+	public Date getStudentDoB() {
+		return studentDoB;
+	}
+	public void setStudentDoB(Date studentDoB) {
+		this.studentDoB = studentDoB;
+	}
+	public ArrayList<String> getSkillList() {
+		return skillList;
+	}
+	public void setSkillList(ArrayList<String> skillList) {
+		this.skillList = skillList;
+	}
+	public Address getStudentAddress() {
+		return studentAddress;
+	}
+	public void setStudentAddress(Address studentAddress) {
+		this.studentAddress = studentAddress;
+	}
+	
+	
+}
---
+++ SpringMvcFormValidationMessageSource/src/com/technovalley21/controllers/StudentAdmissionController.java
@@ -0,0 +1,68 @@
+package com.technovalley21.controllers;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class StudentAdmissionController {
+	
+	/*
+	 * CustomDateEditor is the property editor class that has been provided
+	 * by the Spring MVC. There are so many property editor class is present 
+	 * in Spring MVC framework by default. Such as dateEditor, fileEditor, mapEditor, numberEditor, & etc
+	 * */
+	
+	@InitBinder
+	public void initBinder(WebDataBinder binder){
+		//binder.setDisallowedFields(new String[] {"age"}); // spring mvc framework will skip this field
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		binder.registerCustomEditor(Date.class, "studentDoB", new CustomDateEditor(sdf, false));
+		
+		//binder.registerCustomEditor(String.class, "name", new StudentNameEditor());
+		
+	}
+	
+	@RequestMapping(value="admissionForm.html", method=RequestMethod.GET)
+	public ModelAndView getAdmissionForm(){
+		ModelAndView mav = new ModelAndView("AdmissionFormPage");
+		
+		return mav;
+	}
+	
+	
+	@ModelAttribute
+	public void extraObjects(Model model1){
+		model1.addAttribute("cmsg", "Common Message");
+	}
+	
+	@RequestMapping(value="submitAdmissionForm.html", method=RequestMethod.POST)
+	public ModelAndView submitAdmissionForm( @Valid @ModelAttribute("student") Student student, BindingResult result){
+		
+		/* catch form related error with BindingResult */
+		if( result.hasErrors() ){
+			ModelAndView mav1 = new ModelAndView("AdmissionFormPage");
+			
+			return mav1;
+		}
+		
+		ModelAndView mav = new ModelAndView("AdmissionSuccess");
+		mav.addObject("msgHeader", "Welcome to Spring MVC Model Attribute Tutorials");
+		
+		return mav;
+	}
+	
+}
